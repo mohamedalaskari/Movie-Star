5febd24a83311f33a6931ccff8675df248b60cb5
Khaled Magdy <Khalid.Magdyy@gmail.com>
2024-12-04 21:24:55 +0200

    edit traits

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SubscriptionDetails;
 use App\Trait\responsetrait as TraitResponsetrait;
 use App\Trait\subscripetrait;
+use Carbon\Carbon;
 use Illuminate\Http\ResponseTrait;
+use Illuminate\Support\Facades\Auth;
+
 
 abstract class Controller
 {
-    use TraitResponsetrait , subscripetrait;
+    use TraitResponsetrait ;
+    public  function Subscripe()
+    {
+        $user_id = Auth::user()->id;
+        $active_subscribtion = SubscriptionDetails::all()->where('user_id', $user_id)->where('expiry_date', '>=', Carbon::now());
+        if(count($active_subscribtion) === 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
